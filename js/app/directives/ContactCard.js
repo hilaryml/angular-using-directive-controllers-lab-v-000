3dd356d5b87d998fdf99a71f59be135a00b01bc7
hilaryml <hilyttle@gmail.com>
2017-02-15 15:35:00 -0500

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -19,8 +19,12 @@ function ContactCard() {
 				'<span class="username">{{ username }}</span>',
 			'</div>'
 		].join(''),
-		restrict: 'E'
+		restrict: 'E',
+		controller: function ($scope) {
+				$scope.username = $scope.username.toLowerCase();
+		}
 	};
+
 }
 
 angular
